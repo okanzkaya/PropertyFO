f78216fe354f2f7bc674e6e4e428a1b0e962c809
Okan Ozkaya <okanozkaya987@gmail.com>
2024-07-01 14:08:46 +0300

    Database issue has been solved

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-// backend/index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 const sequelize = require('./config/database');
 const routes = require('./routes');
 
@@ -27,6 +27,14 @@ app.use(limiter);
 
 // Routes
 app.use('/api', routes);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+
+// Anything that doesn't match the above routes, send back index.html from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+});
 
 // Root URL route
 app.get('/', (req, res) => {
